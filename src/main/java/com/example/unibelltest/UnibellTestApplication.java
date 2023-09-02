4799aaab833874d1c4afc87b92ab7b70a2010e0b
Sokolov Roman <sokolov_r.a@bk.ru>
2023-09-02 19:43:24 +0300

    project start

---
+++ src/main/java/com/example/unibelltest/UnibellTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.unibelltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibellTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnibellTestApplication.class, args);
+    }
+
+}
